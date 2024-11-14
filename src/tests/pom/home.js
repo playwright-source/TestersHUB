8d962ff31e8e9e8219f4b3eeb419f968e2a46e3f
thananJayan <thananjayan1988@gmail.com>
2024-11-14 02:43:10 +0000

    Code refactored with proper structure

---
+++ src/tests/pom/home.js
@@ -0,0 +1,34 @@
+const { expect } = require('@playwright/test')
+class Home {
+
+    constructor(page){
+        this.page = page
+    }
+
+    async loadHomePage(){
+        await this.page.goto(process.env.URL);
+    }
+
+    async clickLogin(){
+        this.element = await this.page.locator("//button[@title='Login' and contains(@class, 'text-primary-hover')]");
+        await this.element.click();
+    }
+
+    async enterUserNameandPwd(){
+        await this.page.fill('input[name="email"]', process.env.USER_NAME);
+        await this.page.fill('input[name="password"]', process.env.PASSWORD);
+    }
+
+    async clickSignIn(){
+        this.signInButton = await this.page.locator('button:has-text("SIGN IN")');
+        await this.signInButton.click();
+    }
+
+    async verifyLandingPage(){
+        await expect(this.page).toHaveURL(/www.corporategear/); 
+    }
+
+    
+}
+
+module.exports = Home
---
+++ src/tests/stepdef/corporategear-login.js
@@ -0,0 +1,28 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+// Step 1: Navigate to Corporate Gear website
+Given('the user navigates to the Corporate Gear website', async function () {
+    await this.home.loadHomePage()
+  
+});
+
+// Step 2: Click on the Login button
+When('the user clicks on the "Login" button', async function () {
+    await this.home.clickLogin()
+});
+
+// Step 3: Enter email and password
+When('the user enters their email and password', async function () {
+    await this.home.enterUserNameandPwd()
+});
+
+// Step 4: Click on the SIGN IN button
+When('the user clicks on the "SIGN IN" button', async function () {
+    await this.home.clickSignIn()
+});
+
+// Step 5: Verify landing on the home page
+Then('the user should be redirected to the landing home page', async function () {
+  // Update the URL check
+    await this.home.verifyLandingPage()
+});
